ef729575e61e07d6d90fb88144269abb758a582f
oliviaalchemy <olivia@alchemytechs.com>
2019-08-07 10:34:07 +0100

    Add project files.

---
+++ Better Calculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Better_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");   //as in + - / *
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+               
+            Console.ReadLine();
+        }
+    }
+}
